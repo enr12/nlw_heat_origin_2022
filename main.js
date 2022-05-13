efb9a35fa913fa10977419115e98694f683b9a2a
Enrico Candido <102111229+enr12@users.noreply.github.com>
2022-05-13 20:50:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const socialMediaUser = {
+  github: 'enr12',
+  instagram: '_enricocandido',
+  facebook: 'profile.php?id=100079846892006'
+}
+
+function changeSocialMediaUser() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${socialMediaUser[social]}`
+  }
+}
+
+changeSocialMediaUser()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${socialMediaUser.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userPhoto.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
